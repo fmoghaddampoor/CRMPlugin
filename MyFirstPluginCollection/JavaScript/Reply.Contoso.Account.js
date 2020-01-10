06b5b47a44ed7e135d8c17b340f31bfd31d47ebc
fmoghaddampoor <f.moghaddampoor@gmail.com>
2020-01-10 15:20:49 +0100

    Commented the action caller

--- MyFirstPluginCollection/JavaScript/Reply.Contoso.Account.js
+++ MyFirstPluginCollection/JavaScript/Reply.Contoso.Account.js
@@ -19,8 +19,32 @@ if (typeof (Reply.contoso) === "undefined") {
 if (typeof (Reply.contoso.account) === "undefined") {
     Reply.contoso.account = {
         btnAlert_OnClick: function () {
-            console.log("On click is called");    
-            Actioncaller.call();
+            console.log("On click is called");  
+            var data =
+            {
+                //Account comes from input process arguments for entity reference type
+                "Account": {
+                    //.acccount is the logial name of account table
+                    "@odata.type": "Microsoft.Dynamics.CRM.account",
+                    //account_id is primary key of account entity
+                    //The id of entity comes with {} so we need to replace them
+                    "accountid": Xrm.Page.data.entity.getId().replace('{', '').replace('}', '')
+                }
+            }
+
+            // called if action caller is failed
+            function onError()
+            {
+                window.Xrm.Navigation.openAlertDialog({ text: error.error.message });
+            }
+
+            // called if action caller s successful
+            function onSuccess()
+            {
+
+            }
+            // call action
+            Actioncaller.call("cr1b8_myprocess", data, null, onError, onSuccess);
         }
     };
 }
