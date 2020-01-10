dae831f024731a816230f75cb8da681d3e0ff963
fmoghaddampoor <f.moghaddampoor@gmail.com>
2020-01-10 11:11:37 +0100

    Added changes and comments to my javascript file over namespaces

--- MyFirstPluginCollection/JavaScript/Reply.Contoso.Account.js
+++ MyFirstPluginCollection/JavaScript/Reply.Contoso.Account.js
@@ -1,19 +1,24 @@
-﻿if (typeof (Reply) === "undefined") {
+﻿/*Hint: Define namespace for functions if its not a duplicated using type of namespad is undefined*/
+
+//Namespace reply functions
+if (typeof (Reply) === "undefined") {
     Reply = {
 
     };
 }
 
+//Namespace Reply.contoso functions
 if (typeof (Reply.contoso) === "undefined") {
     Reply.contoso = {
-
+         
     };
 }
 
+//Namespace Reply.contoso.account functions
 if (typeof (Reply.contoso.account) === "undefined") {
     Reply.contoso.account = {
-        OnLoad: function () {
-                
+        OnClick: function () {
+            console.log("On click is called");      
         }
     };
 }
