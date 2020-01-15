c8434642b917ec98097356f077c3f44d437a5f52
fmoghaddampoor <f.moghaddampoor@gmail.com>
2020-01-15 11:23:09 +0100

    Added Account class a and CsvPersonMapping class

---
+++ CSVDataUploaderUsingTinyCSVParser/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVDataUploader
+{
+    public class Account
+    {
+        public string Name { get; set; }
+
+        public string PrimaryContactId { get; set; }
+
+        public DateTime Telephone1 { get; set; }
+    }
+}
---
+++ CSVDataUploaderUsingTinyCSVParser/CsvPersonMapping.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TinyCsvParser.Mapping;
+
+namespace CSVDataUploader
+{
+    public class CsvPersonMapping : CsvMapping<Account>
+    {
+        public CsvPersonMapping(): base()
+        {
+            MapProperty(0, x => x.Name);
+            MapProperty(1, x => x.PrimaryContactId);
+            MapProperty(2, x => x.Telephone1);
+        }
+    }
+}
